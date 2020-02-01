3627a8830ca99d48f80741b88b05b37d4696b4ee
BruceGliff <ilyaawaA@yandex.ru>
2020-02-01 17:46:25 +0300

    testing system update

--- Test.cpp
+++ Test.cpp
@@ -1,4 +1,4 @@
-// ./programm DataSet/tests/001.dat < DataSet/input/001.inp > DataSet/output/001.out
+// ./program DataSet/tests/001.dat < DataSet/input/001.inp > DataSet/output/001.out
 
 #include "Color.hpp"
 #include <sstream>
@@ -19,8 +19,10 @@ enum class ErrorType
     Ok
 };
 
+ErrorType Test(std::string const & x, std::list<std::string> & WA);
 ErrorType Launch()
 {
+    std::list<std::string> WA;
     std::map<std::string, std::string> tests, input;
 
     std::filesystem::directory_entry const DataPath{"DataSet/"};
@@ -62,78 +64,28 @@ ErrorType Launch()
         return ErrorType::NoInputs;
     if (testsAmount != inputAmount)
         return ErrorType::ErData;
+    for (auto const & x : tests)
+        if (input.find(x.first) == input.end())
+        {
+            std::stringstream errout;
+            errout << '"' << x.first << '"' << " doesn't found in input directory!";
+            std::cerr << Color::Modifier{Color::FG_RED, errout.str().c_str()} << std::endl;
+            return ErrorType::ErData;
+        }
+
+    system("rm DataSet/output/*");
 
     for (auto const & x : tests)
     {
         std::stringstream command;
-        command << "./programm " << x.second << " < " << input[x.first] 
+        command << "./program " << x.second << " < " << input[x.first] 
                 << " > " << "DataSet/output/" << x.first << ".out";
         system(command.str().c_str());
-    }
 
-    return ErrorType::Ok;
-}
- 
-ErrorType Test()
-{
-    std::map<std::string, std::string> check, output;
-    std::filesystem::directory_entry const DataPath{"DataSet/"};
-    if (!DataPath.exists())
-        return ErrorType::NoDataSet;
-
-    for (auto const & entry : std::filesystem::directory_iterator(DataPath))
-    {
-        if (entry == std::filesystem::directory_entry{"DataSet/check"})
+        const auto & ret = Test(x.first, WA);
+        if(ret != ErrorType::Ok)
         {
-            for (auto const & insideDir : std::filesystem::directory_iterator(entry))
-            {
-                std::string const path = insideDir.path().string();
-                size_t const len = path.rfind(".") - 1 - path.rfind("/");
-                std::string name{path, path.rfind("/") + 1, len};
-            
-                check[name] = path;
-            }
-        }
-            
-        if (entry == std::filesystem::directory_entry{"DataSet/output"})
-        {
-            for (auto const & insideDir : std::filesystem::directory_iterator(entry))
-            {
-                std::string const path = insideDir.path().string();
-                size_t const len = path.rfind(".") - 1 - path.rfind("/");
-                std::string name{path, path.rfind("/") + 1, len};
-            
-                output[name] = path;
-            }
-        }
-    }
-
-    int const checkAmount = check.size();
-    int const outputAmount = output.size();
-    if (!checkAmount)
-        return ErrorType::NoCheck;
-    if (checkAmount != outputAmount)
-        return ErrorType::ErData;
-
-    std::list<std::string> WA;
-    for (auto const & x : output)
-    {
-        std::ifstream outputFile{x.second};
-        std::ifstream checkFile{check[x.first]};
-
-        std::stringstream outStream, checkStream;
-        outStream << outputFile.rdbuf();
-        checkStream << checkFile.rdbuf();
-
-        if (!outStream.str().compare(checkStream.str()))
-        {
-            std::cout << Color::Modifier{Color::FG_GREEN, x.first.c_str()} << std::endl;
-        }
-        else
-        {
-            WA.push_back(x.first);
-            std::cout << Color::Modifier{Color::FG_RED, x.first.c_str()} 
-                      << Color::Modifier{Color::FG_RED, " WA"} <<  std::endl;
+            return ret;
         }
     }
     if (WA.size() != 0)
@@ -143,6 +95,37 @@ ErrorType Test()
             std::cout << Color::Modifier{Color::FG_RED, x.c_str()} << ' ';
         std::cout << std::endl;
     }
+    return ErrorType::Ok;
+}
+ 
+ErrorType Test(std::string const & x, std::list<std::string> & WA)
+{
+    std::cout << std::setfill('-') << std::setw(40) << std::left << x;
+    
+        std::stringstream out, check;
+        out << "DataSet/output/" << x << ".out";
+        std::ifstream outputFile{out.str()};
+        check << "DataSet/check/" << x;
+        
+        std::ifstream checkFile; 
+        checkFile.open(check.str(), std::ios_base::in);
+        if (!checkFile.is_open())
+            return ErrorType::NoCheck;
+
+        std::stringstream outStream, checkStream;
+        outStream << outputFile.rdbuf();
+        checkStream << checkFile.rdbuf();
+
+        if (!outStream.str().compare(checkStream.str()))
+        {
+            std::cout << Color::Modifier{Color::FG_GREEN, x.c_str()} << std::endl;
+        }
+        else
+        {
+            WA.push_back(x);
+            std::cout << Color::Modifier{Color::FG_RED, x.c_str()} 
+                      << Color::Modifier{Color::FG_RED, " WA"} <<  std::endl;
+        }
 
     return ErrorType::Ok;    
 }
@@ -169,22 +152,11 @@ int main()
         std::cerr << Color::Modifier{Color::FG_RED, "Input data does not exist!"} << std::endl;
         return 0;
         break;
-    }
-    std::cout << "Testing in process..." << std::endl;
-    switch (Test())
-    {
-    case ErrorType::ErData:
-        std::cerr << Color::Modifier{Color::FG_RED, "Wrong data set!"} << std::endl;
-        return 0;
-        break;
-    case ErrorType::NoDataSet:
-        std::cerr << Color::Modifier{Color::FG_RED, "Data set does not exist!"} << std::endl;
-        return 0;
-        break;
     case ErrorType::NoCheck:
-        std::cerr << Color::Modifier{Color::FG_RED, "Check do not exist!"} << std::endl;
+        std::cerr << Color::Modifier{Color::FG_RED, "Check data does not exist!"} << std::endl;
         return 0;
         break;
     }
     return 0;
+    
 }
